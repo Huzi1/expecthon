f5075bd84c94bf48caa02ccb84b27b91fc741dab
Casper Weiss Bang <master@thecdk.net>
2020-10-05 12:57:41 +0200

    Removed unused imports

--- expecthon/case.py
+++ expecthon/case.py
@@ -1,7 +1,5 @@
 import inspect
 from unittest import TestCase
-from .expect import expect
-from .assumptions import failed
 
 
 class InvalidCallerException(Exception):
@@ -26,6 +24,6 @@ def get_testcase(frame) -> TestCase:
             return get_testcase(frame.f_back)
 
         # This line cannot be reached by cov as we cannot reach it from a testcase
-        raise InvalidCallerException()
+        raise InvalidCallerException() 
 
     return test_case
